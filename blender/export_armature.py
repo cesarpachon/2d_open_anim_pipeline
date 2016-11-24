e4a0620acfc5610d5f0db4e5fb20a7cd31105cce
cesarpachon <cesarpachon@gmail.com>
2016-11-24 17:56:19 -0500

    blender: exporting anims: location x,y with frames

--- blender/export_armature.py
+++ blender/export_armature.py
@@ -20,6 +20,47 @@ def _ea_write_vector2(key, val, out):
     out.write('"%s":{"x":%s, "y":%s}\n'%(key, val[0], val[1]))
     return
 
+def _ea_export_curves(action, out):
+    cv_sep = ""
+    for curve in action.fcurves:
+        bone = curve.data_path.split('"')[1]
+        channel = curve.data_path.split(".")[2]
+        
+        #channel filtering
+        #here we discard or rename some curves
+        export = 0
+        label = None
+        #discard z position channel
+        if channel == "location" and curve.array_index < 2:        
+            export = 1
+            if curve.array_index == 0:
+                label = "pos.x"
+            else: 
+                label = "pos.y"
+        
+        if export:           
+            out.write("%s{\n"%(cv_sep))
+            cv_sep = ","
+            _ea_write_str("bone", bone, out)
+            out.write(",")
+            _ea_write_str("channel", label, out)
+            out.write(",")
+            _ea_write_keyframes(curve, out)
+            out.write("}\n")
+        else:
+            print("skipping channel %s with index %d"%(channel, curve.array_index))
+    return
+
+def _ea_write_keyframes(curve, out):
+    sep = ""
+    out.write('"keyframes":[\n')
+    for frame in curve.keyframe_points:
+        out.write(sep)
+        sep = ","
+        out.write(" [%s, %s]\n"%(frame.co[0], frame.co[1]))
+    out.write(']\n')
+    return
+
 def _ea_export_sprites(obj, bone, out):
     sp_sep = ""
     for child in obj.children:
@@ -34,6 +75,8 @@ def _ea_export_sprites(obj, bone, out):
             out.write("}\n")
     return
 
+#export an armature to a json file with the name of the 
+#armature plus the suffix _armature.json
 def export_armature(name, path):
     global _ea_name 
     _ea_name = name
@@ -65,3 +108,23 @@ def export_armature(name, path):
     out.write("}\n")
     out.close()
     return
+
+#exports an action to a json file with the name of the action
+#and the suffix _animation.json
+def export_action(name, path):
+    action = bpy.data.actions[name]
+    path = bpy.path.abspath(path) + name + "_animation.json"
+    out = open(path, "w")
+    print("exporting action %s as %s"%(name, path))
+    out.write("{\n")
+    _ea_write_str("name", name, out)
+    out.write(",")
+    _ea_write_num("frames", action.frame_range[1]-action.frame_range[0], out)
+    out.write(",")
+    out.write('"curves":[\n')
+    _ea_export_curves(action, out)
+    out.write(']\n')
+    out.write("}\n")
+    out.close()
+    return
+
