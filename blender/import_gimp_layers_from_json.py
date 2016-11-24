99edc3693fc7f2d9055ad068640f1ba4ac80b0c1
cesarpachon <cesarpachon@gmail.com>
2016-11-24 12:16:29 -0500

    blender import: creating planes by layer with material and added at right pos

--- blender/import_gimp_layers_from_json.py
+++ blender/import_gimp_layers_from_json.py
@@ -6,11 +6,12 @@ import os
 import bpy
 import json
 
-_igl_path = ""
-_igl_name = ""
+_igl_path = None
+_igl_name = None
+_igl_material = None
 
 def _igl_load_texture(name):
-    global _igl_path, _igl_name
+    global _igl_path, _igl_name, _igl_material
     imgpath = _igl_path + name + ".png"
     print("texture path: %s"%(imgpath))
     try:
@@ -18,18 +19,32 @@ def _igl_load_texture(name):
     except:
         raise NameError("Cannot load image %s" % imgpath)
     # Create material
-    mat = bpy.data.materials.new(_igl_name+'_material') # Add texture slot for color texture
+    _igl_material = bpy.data.materials.new(_igl_name+'_material') # Add texture slot for color texture
     # Create image texture from image
     cTex = bpy.data.textures.new(_igl_name +'_texture', type = 'IMAGE')
     cTex.image = img
-    mtex = mat.texture_slots.add()
+    mtex = _igl_material.texture_slots.add()
     mtex.texture = cTex
     mtex.texture_coords = 'UV'
     mtex.use_map_color_diffuse = True 
     mtex.use_map_color_emission = True 
     mtex.emission_color_factor = 0.5
     mtex.use_map_density = True 
-    mtex.mapping = 'FLAT' 
+    mtex.mapping = 'FLAT'
+    
+    return
+
+def _igl_add_sprite(name, x, y, w, h):
+    global _igl_material
+    # Create new plane and give it UVs
+    bpy.ops.mesh.primitive_plane_add(location=(x, y, 0))
+    bpy.ops.object.mode_set(mode='EDIT')
+    bpy.ops.uv.smart_project()
+    bpy.ops.object.mode_set(mode='OBJECT')
+    # Add material to current object
+    ob = bpy.context.object
+    me = ob.data
+    me.materials.append(_igl_material)
     return
 
 def import_gimp_layers(json_path):
@@ -51,6 +66,6 @@ def import_gimp_layers(json_path):
             w = layer["width"]
             h = layer["height"]
             print("layer: %s x:%s y:%s w:%s h:%s "%(name, x, y, w, h))
-        
+            _igl_add_sprite(name, x, y, w, h)
     return
 
