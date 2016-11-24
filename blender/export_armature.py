a6a90ea3b544476c9a06bfe35331fa356da10fd5
cesarpachon <cesarpachon@gmail.com>
2016-11-24 16:58:51 -0500

    blender: exporting armature with bones and sprites

--- blender/export_armature.py
+++ blender/export_armature.py
@@ -8,20 +8,46 @@ import json
 
 _ea_name = None
 
+def _ea_write_str(key, val, out):
+    out.write('"%s":"%s"\n'%(key, val))
+    return
+
+def _ea_write_num(key, val, out):
+    out.write('"%s":%s\n'%(key, val))
+    return
+
+def _ea_write_vector2(key, val, out):
+    out.write('"%s":{"x":%s, "y":%s}\n'%(key, val[0], val[1]))
+    return
+
+def _ea_export_sprites(obj, bone, out):
+    sp_sep = ""
+    for child in obj.children:
+        if child.parent_bone == bone.name:
+            out.write("%s{\n"%(sp_sep))
+            sp_sep = ","
+            _ea_write_str("name", child.name, out)
+            out.write(",")
+            _ea_write_vector2("pos", child.location, out)
+            out.write(",")
+            _ea_write_num("rot", child.rotation_euler[2], out)
+            out.write("}\n")
+    return
+
 def export_armature(name, path):
     global _ea_name 
     _ea_name = name
     path = bpy.path.abspath(path) + name + "_armature.json"
     out = open(path, "w")
     print("exporting armature %s as %s"%(_ea_name, path))
-    armature = bpy.data.objects[name].data
+    obj = bpy.data.objects[name]
+    armature = obj.data
     out.write("{\n")
     out.write('"name":"%s",\n'%(name))
     out.write('"bones":[\n')
     bones = armature.bones
     bone_sep = ""
     for bone in bones:
-        print(bone)
         out.write("%s{\n"%(bone_sep))
         bone_sep = ","
         out.write('"name":"%s",\n'%(bone.name))
@@ -32,7 +58,7 @@ def export_armature(name, path):
         out.write('"pos":{"x": %s, "y": %s},\n'%(bone.head[0], bone.head[1]))
         out.write('"rot": %s,\n'%(bone.matrix.to_euler()[2]))
         out.write('"sprites":[\n')
-
+        _ea_export_sprites(obj, bone, out)
         out.write(']\n')
         out.write("}\n")
     out.write("]\n")
