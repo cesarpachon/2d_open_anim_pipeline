7e80d0c55533ec50d61a01a79f9f1c64939e6745
cesarpachon <cesarpachon@gmail.com>
2016-11-24 11:59:45 -0500

    blender import: image is being loaded into blender

--- blender/import_gimp_layers_from_json.py
+++ blender/import_gimp_layers_from_json.py
@@ -2,16 +2,32 @@
 # given a json file with GIMP layer information, 
 # it will create quads that will match the GIMP image
 #
-
+import os
 import bpy
 import json
 
+_igl_path = ""
+
+def _igl_load_texture(name):
+    global _igl_path
+    imgpath = _igl_path + name + ".png"
+    print("texture path: %s"%(imgpath))
+    try:
+        img = bpy.data.images.load(imgpath)
+    except:
+        raise NameError("Cannot load image %s" % imgpath)
+    return
+
 def import_gimp_layers(json_path):
-    print(json_path)
+    global _igl_path
+    print("json path: %s"%(json_path))
+    _igl_path = json_path.replace(bpy.path.basename(json_path), "")
+    print("base path: %s"%(_igl_path))
     with open(json_path) as data_file:    
         data = json.load(data_file)
         json_img = data["img"]
         print("image: %s w:%s h:%s"%(json_img["name"], json_img["width"],  json_img["height"]))
+        _igl_load_texture(json_img["name"])
         json_layers = data["layers"]
         for layer in json_layers:
             name = layer["name"]
