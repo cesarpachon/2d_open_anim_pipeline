db1c2b2b26e07a0fd5dfe0b6f2b716416324d49b
cesarpachon <cesarpachon@gmail.com>
2016-11-24 11:45:49 -0500

    printing layer info

--- blender/import_gimp_layers_from_json.py
+++ blender/import_gimp_layers_from_json.py
@@ -12,6 +12,14 @@ def import_gimp_layers(json_path):
         data = json.load(data_file)
         json_img = data["img"]
         print("image: %s w:%s h:%s"%(json_img["name"], json_img["width"],  json_img["height"]))
+        json_layers = data["layers"]
+        for layer in json_layers:
+            name = layer["name"]
+            x = layer["x"]
+            y = layer["y"]
+            w = layer["width"]
+            h = layer["height"]
+            print("layer: %s x:%s y:%s w:%s h:%s "%(name, x, y, w, h))
         
     return
 
