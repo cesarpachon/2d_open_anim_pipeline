e6e456673e58cb411b1c53ba2ec871a628d9957e
cesarpachon <cesarpachon@gmail.com>
2016-11-25 10:47:03 -0500

    cocos: extending spline moveto action to support oap generic curves

--- cocos2d_js_runtime/oap2D.js
+++ cocos2d_js_runtime/oap2D.js
@@ -54,11 +54,13 @@ var oap2D = (function(cc){
    */ 
   oap2D._initSprites = function(armature, armature_json, atlas_json, res_path){
     var _self = this;
+    // a registry of sprites for quick lookup
+    armature.sprites = {};
     armature_json.bones.forEach(function(jbone){
       jbone.sprites.forEach(function(jsprite){
-        console.log(jsprite);
         var csprite = _self._initSprite(jsprite, atlas_json, res_path);
         armature.bones[jbone.name].addChild(csprite);
+        armature.sprites[jsprite.name] = csprite;
       });
     });
   };
@@ -76,7 +78,6 @@ var oap2D = (function(cc){
     var sprite = cc.Sprite.createWithTexture(path, rect);
     sprite.setPosition(jsprite.pos);
     sprite.setRotation(jsprite.rot);
-    console.log(sprite);
     return sprite;
   };
 
@@ -100,9 +101,69 @@ var oap2D = (function(cc){
    * the oap2D cache. 
    */ 
   oap2D.playAnimation= function(armature, animation_name){
-     
+    var action;  
+    var anim = this.animation_cache[animation_name];
+     if(!anim) throw "oap2D: animation not found in cache";
+     var actions = [];
+     anim.curves.forEach(function(curve){
+      var target;
+      if(curve.bone){
+        target = armature.bones[curve.bone];
+        console.log("target bone " + curve.bone + " channel:"+curve.channel + " " +target);
+      }else if(curve.sprite){
+        target = armature.sprite[curve.sprite];
+        console.log("target sprite " + curve.sprite  + " channel:"+curve.channel+ " " +target);
+      }
+      if(curve.channel === "pos.x"){
+         
+      }else if(curve.channel === "pos.y"){
+        
+      }else if(curve.channel === "rot"){
+        action = new oap2D.ActionAnimCurve(target, curve, anim.frames, 20);
+        //actions.push(action);
+      }else if(curve.channel === "hide"){
+        
+      }
+     });
+     return action; 
+     //return new cc.Spawn(actions);
   };
 
+  /**
+   * we want to override the update mechanism of the original action
+   * to apply to any property, not only position
+   */ 
+  oap2D.ActionAnimCurve = cc.CardinalSplineTo.extend({
+    curve: null,
+
+    /*
+     * @param frames_len: total number of frames of the action this curve belongs to. 
+     *  will be used as the 100% to compute total duration.
+     * @fps: used to transform the frame value of keyframes into a absolute time value.
+     * */
+    ctor: function (target, curve, frames_len, fps/*duration, points, tension*/) {
+         cc.CardinalSplineTo.prototype.ctor.call(this);
+         this.curve = curve;
+         this.target = target;
+         var duration = frames_len /  fps; 
+         var points = [];
+         curve.keyframes.forEach(function(keyframe){
+          points.push({
+            y: keyframe[1],
+            x: (keyframe[0]/frames_len) * duration
+          });
+         });
+         var last = points[points.length-1];
+         points.push({
+           x: duration,
+           y: last.y
+         });
+         console.log(curve.keyframes);
+         console.log(points);
+         console.log("duration:"+duration);
+         this.initWithDuration(duration, points, 0);
+     }
+  });
 
   return oap2D;
 })(cc);
